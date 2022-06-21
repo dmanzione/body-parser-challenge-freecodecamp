10442fb9966742a67c7c5aa438f3237830ce1f0c
Donato <donato.manz.sc@gmail.com>
2022-06-20 20:04:48 -0400

    completed

--- index.js
+++ index.js
@@ -5,14 +5,18 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+
+// app.use(bodyParser.urlencoded({extended: false}));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -20,11 +24,20 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami",function(req,res){
+  let obj = {
+    "ipaddress":req.ip,
+    "language":req.headers["accept-language"],
+    "software":req.headers["user-agent"]
+
+}
+  res.json(obj);
+});
 
 
 // listen for requests :)
